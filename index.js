92f7d954813f9d77328eb3803240d0823fcd44a7
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2017-03-05 19:38:02 -0600

    final version done

--- index.js
+++ index.js
@@ -3,7 +3,49 @@ var course = document.getElementById("course");
 var credits = document.getElementById("credits");
 var grade = document.getElementById("grade");
 
-var submit = document.getElementById("submit");
+var firebaseCoursesRef = firebase.database().ref('courses');
+
+firebaseCoursesRef.on('value', function(snapshot){
+
+    // sum(grade*credit)/totalCredits = gpa
+
+    var totalWeightGrade = 0;  // grade*credits
+    var totalCredits = 0;  // total number of credit hours
+
+    // loop through all the data
+    snapshot.forEach(function(courseSnapshot) {
+        var courseGrade = getGradeEq(courseSnapshot.val().grade);
+        var courseCredits = parseInt(courseSnapshot.val().credits);
+
+        totalWeightGrade += courseGrade*courseCredits;
+        totalCredits += parseInt(courseSnapshot.val().credits);
+    });
+
+    // get a reference to GPA
+    var GPARef = firebase.database().ref('GPA');
+    // calculate GPA
+    var GPA = (totalWeightGrade/totalCredits).toFixed(2);
+    
+    // set GPA in firebase
+    GPARef.set(GPA);
+
+    document.getElementById("gpa").innerHTML = GPA;
+    document.getElementById("totCred").innerHTML = totalCredits;
+
+});
+
+function getGradeEq(grade) {
+    if (grade.toLowerCase() == "a")
+        return 4.0;
+    else if (grade.toLowerCase() == "b")
+        return 3.0;
+    else if (grade.toLowerCase() == "c")
+        return 2.0;
+    else if (grade.toLowerCase() == "d")
+        return 1.0;
+    else
+        return 0;
+}
 
 function send() {
     
@@ -13,19 +55,19 @@ function send() {
     uCredits = credits.value;
     uGrade = grade.value;
 
-    // get the reference to the database
-    var db = firebase.database().ref('courses');
-    // get the reference to the child tree
-    var coursesRef = db.push();
-    coursesRef.set({
+    // push a new node under 'courses' 
+    var newCourse = firebaseCoursesRef.child(uCourse);
+    newCourse.set({
         'code': uCode,
         'course': uCourse,
         'credits': uCredits,
         'grade': uGrade
     });
 
+    // empty input boxes
     code.value = "";
     course.value = "";
     credits.value = "";
     grade.value = "";
+
 }
